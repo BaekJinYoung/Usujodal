db2a3556485522ac2f523386ff17f3aa2ad38b52
BaekJinYoung <time121412@gmail.com>
2024-07-12 15:59:24 +0900

    feat: [api] 상세 이전, 다음 게시물

--- app/Http/Controllers/DetailController.php
+++ app/Http/Controllers/DetailController.php
@@ -12,11 +12,26 @@ use Illuminate\Http\Request;
 
 class DetailController extends Controller
 {
-    private function detailRespond($model, $selectColumns, $id, $incrementViews = false) {
+    private function detailRespond($model, $selectColumns, $id, $incrementViews = false, $prevNext = false) {
         $detail = $model::select($selectColumns)->findOrFail($id);
 
         if ($incrementViews) {
             $detail->increment('views');
+        }
+
+        if ($prevNext) {
+            $prevDetail = $model::select('id', 'title')
+                ->where('id', '<', $detail->id)
+                ->orderBy('id', 'desc')
+                ->first();
+
+            $nextDetail = $model::select('id', 'title')
+                ->where('id', '>', $detail->id)
+                ->orderBy('id', 'asc')
+                ->first();
+
+            $detail->prev = $prevDetail ? $prevDetail : null;
+            $detail->next = $nextDetail ? $nextDetail : null;
         }
 
         return ApiResponse::success($detail);
@@ -31,11 +46,11 @@ class DetailController extends Controller
     }
 
     public function announcement_detail($id) {
-        return $this->detailRespond(Announcement::class, ['id', 'views', 'content'], $id, true);
+        return $this->detailRespond(Announcement::class, ['id', 'views', 'content'], $id, true, true);
     }
 
     public function share_detail($id) {
-        return $this->detailRespond(Share::class, ['id', 'views', 'content'], $id, true);
+        return $this->detailRespond(Share::class, ['id', 'views', 'content'], $id, true, true);
     }
 
     public function question_detail($id) {
