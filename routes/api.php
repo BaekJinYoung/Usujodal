7aaff3ce7e86839a2740607acd0eeeaba15be87d
BaekJinYoung <time121412@gmail.com>
2024-07-15 16:13:31 +0900

    feat: [api]

--- routes/api.php
+++ routes/api.php
@@ -1,13 +1,33 @@
 <?php
 
-use App\Http\Controllers\InquiryController;
+//use App\Http\Controllers\InquiryController;
+use App\Http\Controllers\IndexController;
+use App\Http\Controllers\DetailController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::controller(InquiryController::class)->group(function () {
-    Route::post('/inquiry', 'store');
-});
+//Route::controller(InquiryController::class)->group(function () {
+//    Route::post('/inquiry', 'store');
+//});
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::controller(IndexController::class)->group(function () {
+    Route::get('/history', 'history');
+    Route::get('/company', 'company');
+    Route::get('/youtube', 'youtube');
+    Route::get('/consultant', 'consultant');
+    Route::get('/announcement', 'announcement');
+    Route::get('/share', 'share');
+    Route::get('/question', 'question');
+});
+
+Route::controller(DetailController::class)->group(function () {
+    Route::get('/company/{id}', 'company_detail');
+    Route::get('/youtube/{id}', 'youtube_detail');
+    Route::get('/announcement/{id}', 'announcement_detail');
+    Route::get('/share/{id}', 'share_detail');
+    Route::get('/question/{id}', 'question_detail');
+});
