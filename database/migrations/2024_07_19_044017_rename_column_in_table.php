00a0482615c5e2be0fd3455a6b4eb173af7685d9
BaekJinYoung <time121412@gmail.com>
2024-07-19 13:50:49 +0900

    feat: main_image 칼럼명 변경 -> image

---
+++ database/migrations/2024_07_19_044017_rename_column_in_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('companies', function (Blueprint $table) {
+            $table->renameColumn('main_image', 'image');
+        });
+        Schema::table('youtubes', function (Blueprint $table) {
+            $table->renameColumn('main_image', 'image');
+        });
+        Schema::table('consultants', function (Blueprint $table) {
+            $table->renameColumn('main_image', 'image');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('table', function (Blueprint $table) {
+            //
+        });
+    }
+};
